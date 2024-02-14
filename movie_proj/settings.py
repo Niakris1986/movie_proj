be8f469611b2da65a329c13623700a483b74bc34
Leit_nia <93825112+Niakris1986@users.noreply.github.com>
2024-02-14 23:19:22 +0300

    Update settings.py

--- movie_proj/settings.py
+++ movie_proj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+peg2a$&!c3&wu0pcrsi685_k+qobf^n8ku+8e*6ab)#mf_)pi'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
